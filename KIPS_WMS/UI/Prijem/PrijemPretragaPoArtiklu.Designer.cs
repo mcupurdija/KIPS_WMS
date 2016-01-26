61500def47c37776db399bdd8eb36407caa3a739
Marko Ćupurdija <mcupurdija@gmail.com>
2016-01-26 11:08:07 +0100

    opet

--- KIPS_WMS/UI/Prijem/PrijemPretragaPoArtiklu.Designer.cs
+++ KIPS_WMS/UI/Prijem/PrijemPretragaPoArtiklu.Designer.cs
@@ -30,7 +30,32 @@
         private void InitializeComponent()
         {
             this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(165, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Location = new System.Drawing.Point(0, 29);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(240, 156);
+            this.listBox1.TabIndex = 2;
             // 
             // PrijemPretragaPoArtiklu
             // 
@@ -38,7 +63,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(240, 188);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Menu = this.mainMenu1;
+            this.MinimizeBox = false;
             this.Name = "PrijemPretragaPoArtiklu";
             this.Text = "Pretraga po artiklu";
             this.ResumeLayout(false);
@@ -46,5 +75,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
