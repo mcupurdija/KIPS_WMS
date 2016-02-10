047662a682b1c8241c8b421552751b1628d1bc0c
Petar Milisavljevic <pekaking@gmail.com>
2016-02-10 11:01:51 +0100

    aassadas

--- KIPS_WMS/UI/LagerLista.cs
+++ KIPS_WMS/UI/LagerLista.cs
@@ -6,6 +6,7 @@ using KIPS_WMS.Model;
 using KIPS_WMS.NAV_WS;
 using KIPS_WMS.Web;
 using KIPS_WMS.Properties;
+using KIPS_WMS.Data;
 
 namespace KIPS_WMS.UI
 {
@@ -84,38 +85,37 @@ namespace KIPS_WMS.UI
 
         private void tbSifra_KeyUp(object sender, KeyEventArgs e)
         {
-            //Ucitavanje bar-koda TO DO
-            //if (e.KeyCode == Keys.Enter)
-            //{
-            //    var code = SQLiteHelper.oneRowQuery(DbStatements.FindItemsStatementBarcode, new object[] { tbSifra.Text });
+            if (e.KeyCode == Keys.Enter)
+            {
+                var code = SQLiteHelper.oneRowQuery(DbStatements.FindItemsStatementBarcode, new object[] { tbSifra.Text });
 
-            //    string csvLagerList = string.Empty;
-            //    _ws.GetItemLagerList(code[1].ToString(), string.Empty, string.Empty, string.Empty, ref csvLagerList);
-            //    ItemLagerListModel[] lagerList;
-            //    var engine = new FileHelperEngine(typeof(ItemLagerListModel));
-            //    lagerList = (ItemLagerListModel[])engine.ReadString(csvLagerList);
+                string csvLagerList = string.Empty;
+                _ws.GetItemLagerList(code[1].ToString(), string.Empty, string.Empty, string.Empty, ref csvLagerList);
+                ItemLagerListModel[] lagerList;
+                var engine = new FileHelperEngine(typeof(ItemLagerListModel));
+                lagerList = (ItemLagerListModel[])engine.ReadString(csvLagerList);
 
-            //    lvLagerLista.Clear();
-            //    lvLagerLista.View = View.Details;
-            //    lvLagerLista.Columns.Add("Šifra", 60, HorizontalAlignment.Center);
-            //    lvLagerLista.Columns.Add("Naziv", 195, HorizontalAlignment.Center);
-            //    lvLagerLista.Columns.Add("JM", 60, HorizontalAlignment.Center);
-            //    lvLagerLista.Columns.Add("Ukupno", 80, HorizontalAlignment.Center);
-            //    lvLagerLista.Columns.Add("Raspoloživo", 80, HorizontalAlignment.Center);
+                lvLagerLista.Clear();
+                lvLagerLista.View = View.Details;
+                lvLagerLista.Columns.Add("Šifra", 60, HorizontalAlignment.Center);
+                lvLagerLista.Columns.Add("Naziv", 195, HorizontalAlignment.Center);
+                lvLagerLista.Columns.Add("JM", 60, HorizontalAlignment.Center);
+                lvLagerLista.Columns.Add("Ukupno", 80, HorizontalAlignment.Center);
+                lvLagerLista.Columns.Add("Raspoloživo", 80, HorizontalAlignment.Center);
 
-            //    foreach (ItemLagerListModel item in lagerList)
-            //    {
-            //        ListViewItem lvi;
+                foreach (ItemLagerListModel item in lagerList)
+                {
+                    ListViewItem lvi;
 
-            //        lvi =
-            //            new ListViewItem(new[]
-            //            {
-            //                item.WarehouseCode, item.WarehouseName, item.UnitOfMeasure, item.TotalQuantity.ToString(), item.AvailableQuantity.ToString()
-            //            });
+                    lvi =
+                        new ListViewItem(new[]
+                        {
+                            item.WarehouseCode, item.WarehouseName, item.UnitOfMeasure, item.TotalQuantity.ToString(), item.AvailableQuantity.ToString()
+                        });
 
-            //        lvLagerLista.Items.Add(lvi);
-            //    }
-            //}
+                    lvLagerLista.Items.Add(lvi);
+                }
+            }
         }
 
         private void toolBar1_ButtonClick(object sender, ToolBarButtonClickEventArgs e)
