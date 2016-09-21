248f5cf7e486f845db5b47fd1c92793675edc463
Petar Milisavljevic <pekaking@gmail.com>
2016-09-21 15:10:39 +0200

    Ucitavanje credentials-a za NAV iz fajla

--- KIPS_WMS/Web/WebServiceFactory.cs
+++ KIPS_WMS/Web/WebServiceFactory.cs
@@ -1,4 +1,7 @@
 ﻿using KIPS_WMS.NAV_WS;
+using System.Net;
+using System.IO;
+using System;
 
 namespace KIPS_WMS.Web
 {
@@ -8,7 +11,47 @@ namespace KIPS_WMS.Web
 
         static WebServiceFactory()
         {
-            WebService = new MobileWMSSync { Url = Utils.KipsNavWsUrlTest2016, Credentials = Utils.KipsCredentials, PreAuthenticate = true };
+            string file = Utils.GetCurrentDirectory() + "\\DATA\\config.txt";
+            if (File.Exists(file))
+            {
+                try
+                {
+                    StreamReader sr = new StreamReader(file);
+                    string text = sr.ReadToEnd();
+                    string[] lines = text.Split(new Char[] { '\n', '\r' });
+
+                    string _username = "";
+                    string _password = "";
+                    string _domain = "";
+                    foreach (string line in lines)
+                    {
+                        if (line.StartsWith("username"))
+                        {
+                            _username = line.Substring(line.IndexOf('=') + 1);
+                        }
+                        if (line.StartsWith("password"))
+                        {
+                            _password = line.Substring(line.IndexOf('=') + 1);
+                        }
+                        if (line.StartsWith("domain"))
+                        {
+                            _domain = line.Substring(line.IndexOf('=') + 1);
+                        }
+                    }
+
+                    NetworkCredential KipsCredentials = new NetworkCredential(_username, _password, _domain);
+
+                    WebService = new MobileWMSSync { Url = Utils.KipsNavWsUrlTest2016, Credentials = KipsCredentials, PreAuthenticate = true };
+                }
+                catch (Exception e)
+                {
+                    Utils.GeneralExceptionProcessing(e);
+                }
+            }
+            else
+            {
+                Utils.GeneralExceptionProcessing(new Exception("Nije pronađen konfiguracioni fajl."));
+            }
         }
 
         public static MobileWMSSync GetWebService()
